b7fe6e76cc1324ecc0ca9c998f5ca38c7635d2a1
berk <berk_baybas@hotmail.com>
2022-07-10 20:10:17 +0300

    start useWordle hook and add json server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+
+const API_URL = 'http://localhost:3001/solutions'
 
 function App() {
+  const [solutions, setSolutions] = useState(null)
+
+  useEffect(() => {
+    const getWords = async () => {
+      fetch(API_URL)
+        .then((res) => res.json())
+        .then((data) => {
+          const randomSolution = data[Math.floor(Math.random() * data.length)]
+          console.log(randomSolution)
+          setSolutions(randomSolution)
+        })
+    }
+    getWords()
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>wordle</h1>
+      {solutions && <h3>Günün Kelimesi: {solutions.word}</h3>}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
